9bdadcb8822ea65b85adec24349fe605269c457d
Grover Lee <bruce.lee@encora.com>
2024-10-15 23:26:45 -0500

    add title buttons

--- src/extension.ts
+++ src/extension.ts
@@ -22,6 +22,22 @@ export function activate(context: vscode.ExtensionContext) {
     "fileNestingExplorer",
     fileNestingProvider
   );
+
+  vscode.commands.registerCommand("fileNestingExplorer.newFile", () => {
+    vscode.window.showInformationMessage("New file!");
+  });
+
+  vscode.commands.registerCommand("fileNestingExplorer.newFolder", () => {
+    vscode.window.showInformationMessage("New folder!");
+  });
+
+  vscode.commands.registerCommand("fileNestingExplorer.refresh", () => {
+    vscode.window.showInformationMessage("Refreshing!");
+  });
+
+  vscode.commands.registerCommand("fileNestingExplorer.collapse", () => {
+    vscode.window.showInformationMessage("Collapsing!");
+  });
 }
 
 // This method is called when your extension is deactivated
